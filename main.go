d28dc2e6dde3d1dc86350294f386c440bfba4565
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-06-07 14:19:37 +0200

    restructure

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"context"
-	"github.com/a-h/templ"
-	"html/template"
-	"log"
-	"net/http"
-	 
-
-)
+import ("templeruins/web")
 
 func main() {
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	home := Home()
-	homeHTML, err := templ.ToGoHTML(context.Background(), home)
-	if err != nil {
-		log.Fatalf("failed to convert to html: %v", err)
-	}
-	
-	index := template.Must(template.ParseFiles("index.html"))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		index.Execute(w, homeHTML)
-	})
-
-	
-
-	
-
-	http.ListenAndServe(":8080", nil)
+	web.Init()
 }
